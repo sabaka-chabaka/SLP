ca2c03354bc55d3fa0f5cb5a436d8f5f6e1c97c9
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-05 16:55:41 +0400

    bambambimbim

--- SLP.Items/EnergyDrink/EnergyDrinkModule.cs
+++ SLP.Items/EnergyDrink/EnergyDrinkModule.cs
@@ -1,9 +1,14 @@
+#nullable enable
 using System;
 using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Features;
 using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Items;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
 using Exiled.Events.EventArgs.Player;
+using MEC;
 using SLP.Core;
 
 namespace SLP.Items.EnergyDrink;
@@ -49,4 +54,21 @@ public class EnergyDrinkItem : CustomItem
             }
         ]
     };
+
+    protected override void OnAcquired(Player player, Item item, bool displayMessage)
+    {
+        if (TryGet(item, out CustomItem? itema) && itema is not EnergyDrinkItem) return;
+        
+        Timing.CallDelayed(1.0f, () =>
+        {
+            player.Heal(100);
+            player.DisableAllEffects();
+            player.EnableEffect(EffectType.Invigorated, 10, 10);
+            player.EnableEffect(EffectType.MovementBoost, 60, 10);
+            player.EnableEffect(EffectType.Burned, 10, 10);
+            Timing.CallDelayed(10.0f, () => player.EnableEffect(EffectType.CardiacArrest, 1, 5));
+        });
+        
+        base.OnAcquired(player, item, displayMessage);
+    }
 }
