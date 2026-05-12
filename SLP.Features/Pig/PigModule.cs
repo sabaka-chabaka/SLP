937dddc264b0307e16f22cec6bf27dca5722b2e9
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-12 15:26:30 +0400

    traktor

--- SLP.Features/Pig/PigModule.cs
+++ SLP.Features/Pig/PigModule.cs
@@ -1,8 +1,20 @@
+using System;
 using SLP.Core;
 
 namespace SLP.Features.Pig;
 
 public class PigModule : Module
 {
-    
+    public override string Name => "Pig";
+    public override Version Version => new(1, 0, 0);
+
+    public override void OnEnabled()
+    {
+        base.OnEnabled();
+    }
+
+    public override void OnDisabled()
+    {
+        base.OnDisabled();
+    }
 }
