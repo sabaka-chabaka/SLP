acf512aa8b6cd7e923c0faf90189d805e7fee4c0
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-12 22:04:10 +0400

    command

--- SLP.Features/Commands/CommandsModule.cs
+++ SLP.Features/Commands/CommandsModule.cs
@@ -1,4 +1,7 @@
 using System;
+using CommandSystem;
+using Exiled.API.Enums;
+using Exiled.API.Features;
 using SLP.Core;
 
 namespace SLP.Features.Commands;
@@ -7,4 +10,26 @@ public class CommandsModule : Module
 {
     public override string Name => "Commands";
     public override Version Version => new(1, 0, 0);
+}
+
+[CommandHandler(typeof(ClientCommandHandler))]
+public class ScanCommand : ICommand
+{
+    public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+    {
+        var player = Player.Get(sender);
+        
+        if (player.CurrentRoom.Type != RoomType.EzIntercom)
+        {
+            response = "Вы не в интеркоме";
+            return false;
+        }
+        
+        response = "Готово";
+        return true;
+    }
+
+    public string Command { get; } = "scan";
+    public string[] Aliases { get; } = []; 
+    public string Description { get; } = "Начинает сканирование комплекса";
 }
